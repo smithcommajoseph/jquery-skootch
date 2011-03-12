856351bdf9a070ead6e7a08fbed5a6770cb13f25
Joseph Smith <joseph@TechnicolorEnvy.local>
2011-03-11 22:19:41 -0500

    juggling some vars and commenting out some features that we aren't using yet

--- jquery.skootch.js
+++ jquery.skootch.js
@@ -12,6 +12,9 @@
         var defaults = {
             trigger: '#skootch-trigger',
             invader: '#skootch-invader',
+            invaderlinks: '#skootch-invader a',
+            invaderclickretreat: true,
+            invaderclickcallback: function(){},
             wrapperSuffix: 'skootch-wrap',
             direction: "left",
             advancespeed: 'slow',
@@ -48,7 +51,10 @@
         triggeropen = 'skootch-trigger-open',
         
         //set our trigger jQ ob
-        $trigger = $(params.trigger),
+        trigger = params.trigger,
+        
+        //
+        invaderlinks = params.invaderlinks,
         
         //set $indigen to 'this'
         $indigen = this,
@@ -83,31 +89,30 @@
         
         //advance
         advance = function(animatecallback){
-            $trigger.data('state', 'Open');
+            $(trigger).data('state', 'Open');
             
-            var winwidth = $(window).width();
-            var batwidth = $indigen.width();
-            var gutterwidth = (winwidth - batwidth) / 2;
+            // var winwidth = $(window).width();
+            // var batwidth = $indigen.width();
+            // var gutterwidth = (winwidth - batwidth) / 2;
             
             // if(gutterwidth < 200){
-                var fullw = invaderWidth - gutterwidth;
-                 var one4th = Math.floor(fullw * 0.25);
-                 var three4th = one4th * 3; 
-                 $('body').css({"overflow-x": "hidden"});
-                 
+                // var fullw = invaderWidth - gutterwidth;
+                //  var one4th = Math.floor(fullw * 0.25);
+                //  var three4th = one4th * 3; 
+                $('body').css({"overflow-x": "hidden"});
                 $indigen.css('position', 'relative').animate(animap.a, params.advancespeed, params.advanceeasing, function(){
                     // console.log(params.direction);
                 });
             // } 
             
-            $trigger.removeClass(triggerclosed).addClass(triggeropen);
+            $(trigger).removeClass(triggerclosed).addClass(triggeropen);
             $(params.invader).animate(animap.a, params.advancespeed, params.advanceeasing, animatecallback);
         },
         
         //retreat
         retreat = function(animatecallback){
             // debug.log('toggle OFF');
-            $trigger.data('state', 'Closed');
+            $(trigger).data('state', 'Closed');
             
             // if(gutterwidth < 200){        
                 $indigen.animate(animap.r, params.retreatspeed, params.retreateasing, function(){
@@ -115,8 +120,15 @@
                 });
             // }
             
-            $trigger.removeClass(triggeropen).addClass(triggerclosed);
+            $(trigger).removeClass(triggeropen).addClass(triggerclosed);
             $(params.invader).animate(animap.r, params.retreatspeed, params.retreateasing, animatecallback);
+        },
+        
+        //currently unused,
+        // something similar to ui.tabs destroy is probably more appropriate.
+        tidy = function(){
+            $indigen.removeAttr('style');
+            $(params.invader).removeAttr('style');
         };
         
         // set animap = to our desired direction map
@@ -125,35 +137,63 @@
             case 'right':   animap = rightmap;  break;
             case 'top':     animap = topmap;    break;
         }
-    
         
         var clickHandler = function(e){
+            var isinvader = false;
+            
             //unbind
-            $trigger.unbind('.skootchEvents');
+            $(trigger).unbind('.skootchEvents');
             
-            if(typeof $trigger.data('state') == 'undefined'){ 
-                $trigger.data({'state': 'Closed', 'direction': params.direction}); 
+            //set the isinvader true if the e.target in the invaderlinks obj
+            for(var i=0; i < $(params.invaderlinks).length; i++){
+                if(e.target === $(params.invaderlinks)[i]) { isinvader = true; }
             }
             
-            if($trigger.data('state') == 'Closed'){
-                advance(function(){
-                    // rebind                   
-                    $trigger.bind('click.skootchEvents', clickHandler);
-                    
-                });
-            }
-            else{
+            //if if the e.target in the params.invaderlinks obj and we are supposed to retreat on click
+            //call retreat and act appropriately
+            if(isinvader === true && params.invaderclickretreat === true) {
                 retreat(function(){
-                    // rebind
-                    $trigger.bind('click.skootchEvents', clickHandler);
+                    
+                    //fire our callback
+                    params.invaderclickcallback();
+                    
+                    //if we are clicking on something with an href set the window.location
+                    //otherwise, rebind our click
+                    //this is a temp hack as most of this should prob be set in our above callback
+                    if($(e.target).attr('href') !== '' || typeof $(e.target).attr('href') !== 'undefined' ){
+                        window.location = $(e.target).attr('href');
+                    } else {
+                        $(trigger).bind('click.skootchEvents.trigger', clickHandler);
+                    }
                 });
             }
             
+            //
+            else {
+                //initial pass
+                if(typeof $(trigger).data('state') == 'undefined'){ 
+                    $(trigger).data({'state': 'Closed', 'direction': params.direction});
+                }
+                //if we are closed, advance
+                if($(trigger).data('state') == 'Closed'){
+                    advance(function(){
+                        // rebind
+                        $(trigger).bind('click.skootchEvents.trigger', clickHandler);
+                    });
+                }
+                //if we are open, retreat
+                else{
+                    retreat(function(){
+                        // rebind
+                        $(trigger).bind('click.skootchEvents.trigger', clickHandler);
+                    });
+                }
+            }
+            
+            return false;
         };
         
-        // console.log(property);
-                
-        return $trigger.bind('click.skootchEvents', clickHandler);
+        return $(trigger+', '+params.invaderlinks).bind('click.skootchEvents.trigger', clickHandler);
         
     };
   
