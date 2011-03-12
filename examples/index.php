ff218a3f92d61317689932f9a0a3b96bdd3119b9
Joseph Smith <joseph@TechnicolorEnvy.local>
2011-03-11 22:17:35 -0500

    breaking example pages into includes

---
+++ examples/index.php
@@ -0,0 +1,10 @@
+<?php
+    $direction = 'left';
+    require("header.inc");
+?>
+    <script type="text/javascript">
+        $('#body').skootch();
+    </script>
+<?php
+    require("footer.inc");
+?>
---
+++ examples/right.php
@@ -0,0 +1,10 @@
+<?php
+    $direction = 'right';
+    require("header.inc");
+?>
+    <script type="text/javascript">
+        $('#body').skootch('direction', 'right');
+    </script>
+<?php
+    require("footer.inc");
+?>
