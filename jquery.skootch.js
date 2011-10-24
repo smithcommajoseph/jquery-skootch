5355445a676b9f7cdf0cb54eaeb7eff7ba56baf6
Joseph Smith <technicolorenvy@gmail.com>
2011-10-24 09:20:33 -0600

    breaking up into methods

--- jquery.skootch.js
+++ jquery.skootch.js
@@ -12,36 +12,127 @@
 		
 		E_SPACE = '.skooEvents',
 		STATE = 'skooState',
-		OPTS = 'skooOpts';
+		OPTS = 'skooOpts',
 
-	function _setParams(options, arg2){
-		var overrides = {},
-			params = {},
-			opts = {},
-			nextAction = '',
-			nextActionCallback = null;
-	
-		if(typeof options == 'object') { overrides = options; }
-	
-		if(typeof options == 'string') {
-			switch(options){
-				case 'destroy':
-				case 'retreat':
-				case 'advance':
-					nextAction = options;
-					if(arg2 !== undefined && arg2.constructor == Function){ nextActionCallback = arg2; }
-					break;
-				default:
-				if(arg2 !== undefined){
-					for(var prop in $.fn.skootch.defaults){
-						if(prop == options) { overrides[options] = arg2; }
+	methods = {
+		init: function(a,b){
+			return this.each(function(){
+				var $trigger = $(this),
+					params = _setParams(a, b),
+					
+					//the $trigger node wrapper id
+					indigenwrap = $(params.indigen).attr('id')+'-'+params.wrapperSuffix,
+
+					//wrap $params.indigen and set '$indigenwrapper' to the res
+					$indigenwrapper = $(params.indigen).wrap(function() {
+						return '<div id="'+indigenwrap+'" syle="position: relative;"/>';
+					});
+					
+				$trigger.data(OPTS, params);
+								
+				return $('#'+$trigger.attr('id')+', '+params.invaderLinks).bind('click'+E_SPACE, clickHandler = function(e){
+					var isinvader = false,
+						nonVaders = function(){
+							//fire our custom callback if it exists
+							if(params.invaderClickCallback !== null){ params.invaderClickCallback(e); }
+							//otherwise...
+							else {
+								 //if we are clicking on something with an href set the window.location
+								 //otherwise, rebind our click
+								 if($(e.target).attr('href') !== '' || typeof $(e.target).attr('href') !== undefined ){
+								     window.location = $(e.target).attr('href');
+								 }
+								 //not a fan of this logic...
+								 else if($(e.target).attr('type') === 'submit') {
+								     var $form = $(e.target).closest('form');
+								 }
+							
+							}
+						};
+					
+					//unbind
+					$trigger.unbind('click'+E_SPACE);
+				
+					//set the isinvader true if the e.target in the invaderLinks obj
+					for(var i=0; i < $(params.invaderLinks).length; i++){
+					    if(e.target === $(params.invaderLinks)[i]) { isinvader = true; }
 					}
+				
+					//call retreat and act appropriately
+					if(isinvader === true) {
+						$trigger.bind('click'+E_SPACE, clickHandler);
+					
+						if(params.invaderClickRetreat === true){ _retreat($trigger, params, function(){ nonVaders(); });
+						} else { nonVaders(); }
+					} else {
+						//initial pass
+						if(typeof $(params.indigen).data(STATE) == 'undefined' || $(params.indigen).data(STATE) === null){                             
+							$(params.indigen)
+								.data(STATE, 0)
+							 	.data('justify', params.justify);
+						}
+						//if we are closed, advance
+						if($(params.indigen).data(STATE) == 0){
+							_advance($trigger, params, function(){
+								$trigger.bind('click'+E_SPACE, clickHandler);
+							});
+						}
+						//if we are open, retreat
+						else{
+							_retreat($trigger, params, function(){
+								$trigger.bind('click'+E_SPACE, clickHandler);
+							});
+						}
+					}
+			
+					return false;
+				});
+				
+			});
+		},
+		advance: function(a){
+			return this.each(function(){
+				var $trigger = $(this),
+					params = $trigger.data(OPTS),
+					callback = (a !== undefined && a.constructor == Function) ? a : null;
+					
+				_advance($trigger, params, callback);
+			});
+		},
+		retreat: function(a){
+			return this.each(function(){
+				var $trigger = $(this),
+					params = $trigger.data(OPTS),
+					callback = (a !== undefined && a.constructor == Function) ? a : null;
+					
+				_retreat($trigger, params, callback);
+			});
+		},
+		destroy: function(a){
+			return this.each(function(){
+				var $trigger = $(this),
+					params = $trigger.data(OPTS)
+					callback = (a !== undefined && a.constructor == Function) ? a : null;
+					
+				_destroy($trigger, params, callback);
+			});
+		}
+	};
+	
+	function _setParams(a,b){
+		var overrides = {},
+			params = {}, p;
+		
+		if(typeof a == 'object') { overrides = a; }
+		if(typeof a == 'string') {
+			if(b !== undefined){
+				for(p in $.fn.skootch.defaults){
+					if(p == a) { overrides[a] = b; }
 				}
 			}
 		}
-		params = $.extend({}, $.fn.skootch.defaults, overrides);	
-		opts = {nextAction: nextAction, nextActionCallback: nextActionCallback, params: params};
-		return opts;
+		
+		return $.extend({}, $.fn.skootch.defaults, overrides);
 	}
 
 	function _setDirectionMaps($trigger, params){
@@ -88,11 +179,10 @@
 		return animap;
 	}
 
-	function _destroy($trigger, node, callback){
-		var params = $(node).data(OPTS);
+	function _destroy($trigger, params, callback){
 		if(!params) { return false; }
 	
-		$(node).removeData(OPTS);
+		$trigger.removeData(OPTS);
 		$trigger.unbind('click'+E_SPACE);
 		$(params.indigen).unwrap();
 		$(params.invader+', '+params.indigen).removeAttr('style');
@@ -136,103 +226,14 @@
 		}
 	}
 
-	$.fn.skootch = function(option, arg2) {
-		return this.each(function(){
-			var $trigger = $(this),
-				opts = _setParams(option, arg2),
-				params = opts.params || null,
-				nextAction = opts.nextAction || null,
-				nextActionCallback = opts.nextActionCallback || null;
-			
-			$trigger.data(OPTS, params);
-		
-			if(nextAction !== null){
-				switch(nextAction){
-					case 'advance':
-						_advance($trigger, params, nextActionCallback);
-						break;
-					case 'retreat':
-						_retreat($trigger, params, nextActionCallback);
-						break;
-					case 'destroy':
-						_destroy($trigger, this, nextActionCallback);
-						break;
-				}
-			} else {
-				//the $trigger node wrapper id
-				var indigenwrap = $(params.indigen).attr('id')+'-'+params.wrapperSuffix,
-				
-					//wrap $params.indigen and set '$indigenwrapper' to the res
-					$indigenwrapper = $(params.indigen).wrap(function() {
-						return '<div id="'+indigenwrap+'" syle="position: relative;"/>';
-					});
-			
-				return $('#'+$trigger.attr('id')+', '+params.invaderLinks).bind('click'+E_SPACE, clickHandler = function(e){
-					var isinvader = false,
-						nonInvaderClicks = function(){
-							//fire our custom callback if it exists
-							if(params.invaderClickCallback !== null){ params.invaderClickCallback(e); }
-							//otherwise...
-							else {
-								 //if we are clicking on something with an href set the window.location
-								 //otherwise, rebind our click
-								 if($(e.target).attr('href') !== '' || typeof $(e.target).attr('href') !== undefined ){
-								     window.location = $(e.target).attr('href');
-								 }
-								 //not a fan of this logic...
-								 else if($(e.target).attr('type') === 'submit') {
-								     var $form = $(e.target).closest('form');
-								 }
-							
-							}
-						};
-					
-					//unbind
-					$trigger.unbind('click'+E_SPACE);
-				
-					//set the isinvader true if the e.target in the invaderLinks obj
-					for(var i=0; i < $(params.invaderLinks).length; i++){
-					    if(e.target === $(params.invaderLinks)[i]) { isinvader = true; }
-					}
-				
-					//call retreat and act appropriately
-					if(isinvader === true) {
-						$trigger.bind('click'+E_SPACE, clickHandler);
-					
-						if(params.invaderClickRetreat === true){
-							_retreat($trigger, params, function(){ nonInvaderClicks(); });
-						} else {
-							nonInvaderClicks();
-						}
-					} else {
-						//initial pass
-						if(typeof $(params.indigen).data(STATE) == 'undefined' || $(params.indigen).data(STATE) === null){                             
-							$(params.indigen)
-								.data(STATE, 0)
-							 	.data('justify', params.justify);
-						}
-						//if we are closed, advance
-						if($(params.indigen).data(STATE) == 0){
-							_advance($trigger, params, function(){
-								// rebind
-								$trigger.bind('click'+E_SPACE, clickHandler);
-							});
-						}
-						//if we are open, retreat
-						else{
-							_retreat($trigger, params, function(){
-								// rebind
-								$trigger.bind('click'+E_SPACE, clickHandler);
-							});
-						}
-					}
-			
-					return false;
-				});
-			}
-		});
+	$.fn.skootch = function(method) {
+		if (methods[method]) {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else {
+			return methods.init.apply(this, arguments);
+		}
 	};
-
+	
 	$.fn.skootch.ver = function() { return ver; };
 
 	$.fn.skootch.defaults = {
